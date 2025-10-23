204bdb4149e5230c1622644d3bf134c845a1a6dc
bzbzz <nathan.thuault@protonmail.com>
2025-10-23 15:18:18 +0200

    nvim: added rust lsp

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,7 +1,7 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "rust-analyzer", "vtsls", "cssls", "clangd",  "pyright", "jdtls" }
+local servers = { "rust_analyzer", "vtsls", "cssls", "clangd",  "pyright", "jdtls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
