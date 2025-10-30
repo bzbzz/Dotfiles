4f5563757263d878025c21993dae788015c4fc79
bzbzz <nathan.thuault@protonmail.com>
2025-10-30 14:30:16 +0100

    pandoc: headers for codeblocks

--- .pandoc/filters/header-line.lua
+++ .pandoc/filters/header-line.lua
@@ -1,12 +1,6 @@
 function Header(el)
-	local heading_text = pandoc.utils.stringify(el.content)
-	return {
-		el,
-		pandoc.RawBlock(
-			"latex",
-			"\\vspace{-1em}\\noindent{\\color[gray]{0.5}\\rule{\\widthof{\\textbf{"
-				.. heading_text
-				.. "}} + 2em}{0.2pt}}"
-		),
-	}
+  return {
+    el,
+    pandoc.RawBlock('latex', '\\vspace{-1em}\\noindent{\\color[gray]{0.5}\\rule{\\textwidth}{0.2pt}}')
+  }
 end
