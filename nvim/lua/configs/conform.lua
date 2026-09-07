2d19a9c2277e812362d9161c235e12f27961aa81
bzbzz <nathan.thuault@protonmail.com>
2026-09-07 21:13:39 +0200

    nvim: unused lsp and formatters gone

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -2,15 +2,7 @@ local options = {
 	formatters_by_ft = {
 		lua = { "stylua" },
 		c = { "clang-format" },
-		css = { "prettier" },
-		html = { "prettier" },
-		typescript = { "prettier" },
-		typescriptreact = { "prettier" },
-		javascript = { "prettier" },
-		javascriptreact = { "prettier" },
         json = { "prettier" },
-		-- php = { "php-cs-fixer" },
-		-- python = { "black" },
 	},
 
 	-- format_on_save = {
